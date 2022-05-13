a252e6226acfecaf99562d27553a7e6078358d8a
AH-dark <ahdark@outlook.com>
2022-05-13 23:50:49 +0800

    Doc: homepage description

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -15,10 +15,10 @@ const Homepage: React.FC = () => {
                 {"Fuck Your Mom"}
             </Typography>
             <Typography component={"p"} variant={"body1"} margin={1}>
-                There is a home page that explains what the project does, what's interesting about
+                There is a homepage that explains what the project does, what's interesting about
                 this project, and how to get involved in this project.
                 <br />
-                You can apply for the corresponding subdomain by sending an email to{" "}
+                You can apply for the corresponding subdomain by sending email to{" "}
                 <Link href={"mailto:admin@fuckur.mom"} underline={"hover"}>
                     {"admin@fuckur.mom"}
                 </Link>{" "}
@@ -26,9 +26,10 @@ const Homepage: React.FC = () => {
             </Typography>
             <Typography component={"p"} variant={"body1"} margin={1}>
                 Don't get me wrong, we have all the respect for your mother. This project gives you
-                the freedom to create by distributing subdomains to you to achieve the bashing of
-                idiots. We firmly believe that bashing idiots can change idiots or prompt idiots to
-                self-harm and commit suicide, thereby reducing the number of idiots in the world.
+                the freedom to create by distributing subdomains to you, which may achieve the
+                bashing of idiots. We firmly believe that bashing idiots can change idiots or prompt
+                idiots to self-harm and commit suicide, thereby reducing the number of idiots in the
+                world.
             </Typography>
         </Card>
     );
