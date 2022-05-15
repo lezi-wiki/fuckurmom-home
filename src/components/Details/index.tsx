e4c14a66abc0e07b476dd90b9e04c2ad97b6ae85
AH-dark <ahdark@outlook.com>
2022-05-15 17:35:06 +0800

    Doc: Optimize the rendering of <ul>

--- src/components/Details/index.tsx
+++ src/components/Details/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import {
+    Box,
     Button,
     Dialog,
     DialogActions,
@@ -35,7 +36,7 @@ const Details: React.FC = () => {
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
-                "& p": {
+                "& p,ul,li": {
                     textAlign: "left",
                     width: "95%",
                 },
@@ -48,57 +49,52 @@ const Details: React.FC = () => {
             <Typography component={"p"} variant={"body1"} margin={1}>
                 Before you submit, you need to make sure your situation meets our requirements. We
                 have the following requirements for submitted applications:
-                <ul>
-                    <li>
-                        You must open source the code of the applications you deploy using the
-                        subdomain under any license.
-                    </li>
-                    <li>
-                        You may not monetize the applications deployed by the subdomain in any way,
-                        except for non-mandatory donations.
-                    </li>
-                    <li>
-                        You are not allowed to deploy service projects such as IDC and Image Storage
-                        Service in subdomains. However, display sites such as personal blogs are
-                        allowed.
-                    </li>
-                    <li>
-                        Subdomains with specific meanings such as www, ftp, blog, etc. cannot be
-                        applied for.
-                    </li>
-                    <li>
-                        You need to keep your applications running for a long time. In the event of
-                        multiple service interruptions, we will cancel the resolution of your
-                        subdomain.
-                    </li>
-                    <li>You must deploy your applications legally, not from your home PC.</li>
-                </ul>
             </Typography>
+            <Box component={"ul"} margin={0}>
+                {[
+                    "You must open source the code of the applications you deploy using the subdomain under any license.",
+                    "You may not monetize the applications deployed by the subdomain in any way, except for non-mandatory donations.",
+                    "You are not allowed to deploy service projects such as IDC and Image Storage Service in subdomains. However, display sites such as personal blogs are allowed.",
+                    "Subdomains with specific meanings such as www, ftp, blog, etc. cannot be applied for.",
+                    "You need to keep your applications running for a long time. In the event of multiple service interruptions, we will cancel the resolution of your subdomain.",
+                    "You must deploy your applications legally, not from your home PC.",
+                ].map((text, index) => (
+                    <Box component={"li"} key={index}>
+                        {text}
+                    </Box>
+                ))}
+            </Box>
             <Typography component={"p"} variant={"body1"} margin={1}>
                 There are strict formatting requirements to follow for your application:
-                <ul>
-                    <li>You need to point out what the idiot did, write it as best you can!</li>
-                    <li>
-                        You need to identify yourself and leave your contact details so we can
-                        better communicate with you.
-                    </li>
-                    <li>
-                        If authenticated, you have unlimited access to subdomains which begin with
-                        your name, which is discouraged but considered cool.
-                    </li>
-                </ul>
             </Typography>
+            <Box component={"ul"} margin={0}>
+                {[
+                    "You need to point out what the idiot did, write it as best you can!",
+                    "You need to identify yourself and leave your contact details so we can better communicate with you.",
+                    "If authenticated, you have unlimited access to subdomains which begin with your name, which is discouraged but considered cool.",
+                ].map((text, index) => (
+                    <Box component={"li"} key={index}>
+                        {text}
+                    </Box>
+                ))}
+            </Box>
             <Typography component={"p"} variant={"body1"} margin={1}>
                 The application format is as follows:
-                <ul>
-                    <li>Your name</li>
-                    <li>Your contact</li>
-                    <li>Your project name</li>
-                    <li>The type of subdomain you are applying for</li>
-                    <li>What idiots do (detailed description)</li>
-                    <li>Idiots' contact</li>
-                </ul>
             </Typography>
+            <Box component={"ul"} margin={0}>
+                {[
+                    "Your name",
+                    "Your contact",
+                    "Your project name",
+                    "The type of subdomain you are applying for",
+                    "What idiots do (detailed description)",
+                    "Idiots' contact",
+                ].map((text, index) => (
+                    <Box component={"li"} key={index}>
+                        {text}
+                    </Box>
+                ))}
+            </Box>
             <Button size={"large"} onClick={handleOpen}>
                 {"Go for apply"}
             </Button>
