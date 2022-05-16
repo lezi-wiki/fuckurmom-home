ce7f1de192ec6122d3d90c8189d7ddb5a90b4c13
bilibilixkz <xkz@sky233.com>
2022-05-16 21:54:21 +0800

    Update index.ts

--- src/lang/translation/index.ts
+++ src/lang/translation/index.ts
@@ -4,12 +4,14 @@ import enUS from "./en-US";
 import zhCN from "./zh-CN";
 import zhHK from "./zh-HK";
 import zhTW from "./zh-TW";
+import esMX from "./es-MX";
 
 const resources: Resource = {
     "en-US": enUS,
     "zh-CN": zhCN,
     "zh-HK": zhHK,
     "zh-TW": zhTW,
+    "es-MX": esMX,
 };
 
 export default resources;
