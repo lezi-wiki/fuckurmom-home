a6f2e8b44f9b8a981da66a7edbd6d3b2b8f310e2
Bruce Tian <65952137+Bruce081@users.noreply.github.com>
2022-05-16 20:44:51 +0800

    Translate: zh-TW translation by bruce081 (#5)
    * Add zh-TW translate
    * Add:    "zh-TW": "台灣正體（台湾）"
    * Update index.ts

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -29,6 +29,7 @@ export const languages: Record<keyof typeof resources, string> = {
     "en-US": "English (US)",
     "zh-CN": "简体中文（中国大陆）",
     "zh-HK": "繁體中文（香港）",
+    "zh-TW": "台灣正體（台湾）",
 };
 
 export default i18n;
---
+++ src/lang/translation/zh-TW.ts
@@ -0,0 +1,69 @@
+import TranslationModel from "./model";
+
+const zhTW: TranslationModel = {
+    HomeContext: {
+        "Fuck Your Mom": "Fuck Your Mom",
+        "There is a homepage that explains what the project does, what's interesting about this project, and how to get involved in this project.":
+            "這是一個主頁用於介紹這個專案的作用、這個專案的有趣之處，以及如何參與這個專案。",
+        "You can apply for the corresponding subdomain by submit an Issue, but you need to clarify your identity and the reason for the application.":
+            "你可以透過提交Issue來申請對應的子域，單是您需要表明自己的身分和申請的原因。",
+        "Don't get me wrong, we have all the respect for your mother. This project gives you the freedom to create by distributing subdomains to you, which may achieve the bashing of idiots. We firmly believe that bashing idiots can change idiots or prompt idiots to self-harm and commit suicide, thereby reducing the number of idiots in the world.":
+            "不要誤會我們的不要误会我们的宗旨，我們對您的老母抱有極大的敬意。本項目透過提供子域名的方式鼓勵你自由創作，以實現對傻逼的抨擊。我們堅信，足夠的對於傻逼的抨擊可以改變傻逼的內心或促使傻逼自殘和自殺，從而減少世界上傻逼的數量。",
+        "Go for details": "了解詳細資訊",
+    },
+    DetailContext: {
+        Details: "詳細訊息",
+        "Before you submit, you need to make sure your situation meets our requirements. We have the following requirements for submitted applications:":
+            "在您提交之前，您需要確保您的情況符合我們的要求。我們對提交的申請有以下要求：",
+        "You must open source the code of the applications you deploy using the subdomain under any license.":
+            "您必須在任意許可下將您在子域中部署的應用程序的代碼開源。",
+        "You may not monetize the applications deployed by the subdomain in any way, except for non-mandatory donations.":
+            "您不得以任何方式通過子域中部署的應用程序獲利。 （非強制性的捐贈不在內）",
+        "You are not allowed to deploy service projects such as IDC and Image Storage Service in subdomains. However, display sites such as personal blogs are allowed.":
+            "不得在子域中部署IDC、圖片存儲等服務項目。允許部署個人展示頁面例如網誌等網站。",
+        "Subdomains with specific meanings such as www, ftp, blog, etc. cannot be applied for.":
+            "不得申請www、ftp、blog等具有特定含義的子域。",
+        "You need to keep your applications running for a long time. In the event of multiple service interruptions, we will cancel the resolution of your subdomain.":
+            "您需要保證您的應用程序可以長期運行。如果出現多次服務中斷，我們可能取消對您子域的解析。",
+        "You must deploy your applications legally, not from your home PC.":
+            "您必須通過合法方式部署您的應用程序，例如不得通過您的家用電腦部署。",
+        "There are strict formatting requirements to follow for your application:":
+            "您的應用程序需要遵循嚴格的格式要求：",
+        "You need to point out what the idiot did, write it as best you can!":
+            "您需要指出白痴做了什麼，盡你所能地寫吧！",
+        "You need to identify yourself and leave your contact details so we can better communicate with you.":
+            "您需要表明自己的身份並留下您的聯絡方式，以便我們更好地與您交流。",
+        "If authenticated, you have unlimited access to subdomains which begin with your name, which is discouraged but considered cool.":
+            "如果您的身份通過驗證，您可以無限制地獲取以您的名字開頭的子域，雖然我們不鼓勵這樣做，但這確實是很牛逼的。",
+        "The application format is as follows:": "申請格式如下：",
+        "Your name": "您的姓名",
+        "Your contact": "您的聯絡方式",
+        "Your project name": "您的專案名稱",
+        "The type of subdomain you are applying for": "您申請的子域類型（NS/CNAME）",
+        "What idiots do (detailed description)": "傻逼為什麼是傻逼（詳細說明）",
+        "Idiots' contact": "傻逼的聯絡方式",
+        "Go for apply": "前去申請",
+        Confirm: "確認訊息",
+        "You will be navigated to the GitHub Issue page, be sure to state [Apply] at the beginning of the title of the Issue":
+            "您將被指引至 GitHub Issue 網頁，請務必在問題標題的開頭註明 [Apply]",
+        Cancel: "取消",
+        Go: "前去",
+    },
+    pageName: {
+        Home: "主頁",
+        Details: "詳細訊息",
+        "Fuck Your Mom": "Fuck Your Mom",
+    },
+    siteCard: {
+        Site: "網站：",
+        Domain: "域名：",
+        Owner: "擁有者：",
+        Idiot: "傻逼：",
+        Go: "前去",
+    },
+    languageSwitcher: {
+        "System language switched to {{name}}": "系統語言已調整至 {{name}}",
+    },
+};
+
+export default zhTW;
