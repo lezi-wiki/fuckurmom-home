684db61c84b80cb171685c561b0522131c8e4344
Bruce081 <65952137+Bruce081@users.noreply.github.com>
2022-05-16 13:51:58 +0800

    Translate: zh_HK by Bruce081 (#4)
    * Update zh-HK.ts

---
+++ src/lang/translation/zh-HK.ts
@@ -0,0 +1,66 @@
+import TranslationModel from "./model";
+
+const zhHK: TranslationModel = {
+    HomeContext: {
+        "Fuck Your Mom": "Fuck Your Mom",
+        "There is a homepage that explains what the project does, what's interesting about this project, and how to get involved in this project.":
+            "呢個系一個主頁用於闡述呢個計劃之作用、呢個計劃的有趣之處，以及點樣參與呢個計劃。",
+        "You can apply for the corresponding subdomain by submit an Issue, but you need to clarify your identity and the reason for the application.":
+            "你可以通過提交Issue嚟申請對應之子域，但系需要闡明自己的身份和申請的原因。",
+        "Don't get me wrong, we have all the respect for your mother. This project gives you the freedom to create by distributing subdomains to you, which may achieve the bashing of idiots. We firmly believe that bashing idiots can change idiots or prompt idiots to self-harm and commit suicide, thereby reducing the number of idiots in the world.":
+            "请正确理解我哋的初衷，我哋對你的老母抱有極大敬意。本計劃通過提供子域名的方式鼓勵你自由創作，以實現對傻逼的DQ。我哋堅信，對傻逼的DQ如果足夠，可以改變傻逼之內心或促進傻逼自殘和自殺，呢可以改變傻逼在世界上存在之數目。",
+        "Go for details": "了解詳細資訊",
+    },
+    DetailContext: {
+        Details: "詳細訊息",
+        "Before you submit, you need to make sure your situation meets our requirements. We have the following requirements for submitted applications:":
+            "喺你提交之前，你需要確保你的情況符合我哋的要求。我哋對提交的申請有以下要求：",
+        "You must open source the code of the applications you deploy using the subdomain under any license.":
+            "你務必喺任意許可下將你喺子域中部署的應用程序的代碼開源。",
+        "You may not monetize the applications deployed by the subdomain in any way, except for non-mandatory donations.":
+            "你不得以任何方式通過子域中部署的應用程序之獲利。（非夾硬性捐贈不被包含在內）",
+        "You are not allowed to deploy service projects such as IDC and Image Storage Service in subdomains. However, display sites such as personal blogs are allowed.":
+            "不得喺子域中部署增值電訊與雲計算服務、圖像儲存等服務。但部署個人展示頁面諸如網誌等網站系被允許的。",
+        "Subdomains with specific meanings such as www, ftp, blog, etc. cannot be applied for.":
+            "不得申請www、ftp、blog等擁有特定含義的子域",
+        "You need to keep your applications running for a long time. In the event of multiple service interruptions, we will cancel the resolution of your subdomain.":
+            "你需要確保你的應用程式可以長期運行。如果出現多次服務中斷，我哋可能取消你對子域解析之權利。",
+        "You must deploy your applications legally, not from your home PC.":
+            "您必須透過不違法之方式部署您的應用程式，諸如您不可以從您的家用電腦部署服務。",
+        "There are strict formatting requirements to follow for your application:":
+            "您的應用程式需要遵守嚴格的格式要求：",
+        "You need to point out what the idiot did, write it as best you can!":
+            "您需要說明傻逼做了乜？盡你最大努力填寫吧",
+        "You need to identify yourself and leave your contact details so we can better communicate with you.":
+            "您需要說明自己的身分並留下您的聯絡方式，這可以方便我哋更好的與您交流。",
+        "If authenticated, you have unlimited access to subdomains which begin with your name, which is discouraged but considered cool.":
+            "您的身分驗證通過後，您可以沒有限制的獲取以您姓名為開始的子域，我們雖然並不鼓勵這樣的作法，但這實際上很牛逼。",
+        "The application format is as follows:": "申請格式在下方：",
+        "Your name": "您的姓名",
+        "Your contact": "您的聯絡方式",
+        "Your project name": "您的計劃名稱",
+        "The type of subdomain you are applying for": "您申請的子域的類型（NS/CNAME）",
+        "What idiots do (detailed description)": "傻逼為乜係傻逼傻逼为什么系傻逼（詳細表明）",
+        "Idiots' contact": "傻逼的聯絡方式",
+        "Go for apply": "前去申請",
+        Confirm: "確認訊息",
+        "You will be navigated to the GitHub Issue page, be sure to state [Apply] at the beginning of the title of the Issue":
+            "您將被指引到 GitHub Issue 網頁，請一定要在問題標題的開始標註 [Apply]",
+        Cancel: "取消",
+        Go: "前去",
+    },
+    pageName: {
+        Home: "主頁",
+        Details: "詳細訊息",
+        "Fuck Your Mom": "Fuck Your Mom",
+    },
+    siteCard: {
+        Site: "網站：",
+        Domain: "域名：",
+        Owner: "擁有者：",
+        Idiot: "傻逼：",
+        Go: "前去",
+    },
+};
+
+export default zhHK;
