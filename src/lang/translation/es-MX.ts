c1d0fb7ae73cad8192f89b740e1878bac641bfd7
bilibilixkz <xkz@sky233.com>
2022-05-17 09:45:05 +0800

    Add files via upload

---
+++ src/lang/translation/es-MX.ts
@@ -0,0 +1,69 @@
+import TranslationModel from "./model";
+
+const esMX: TranslationModel = {
+    HomeContext: {
+        "Fuck Your Mom": "Fuck Your Mom",
+        "There is a homepage that explains what the project does, what's interesting about this project, and how to get involved in this project.":
+            "Aquí hay inicio página que explica lo que hace el proyecto,interesante información del proyecto y como unirse el proyecto",
+        "You can apply for the corresponding subdomain by submit an Issue, but you need to clarify your identity and the reason for the application.":
+            "Para solicitar un subdominio, usted tiene que enviar un issue, también, su informacion personal y razón por la que necesitas también se necesita.",
+        "Don't get me wrong, we have all the respect for your mother. This project gives you the freedom to create by distributing subdomains to you, which may achieve the bashing of idiots. We firmly believe that bashing idiots can change idiots or prompt idiots to self-harm and commit suicide, thereby reducing the number of idiots in the world.":
+            "Sin ofender, todos tenemos un gran respeto por tu madre. Le daremos el subdominio para garantizar que pueda crear libremente para puede golpear a los tontas emocionalmente. Creemos golpear a los tontas lo suficiente puede cambiarlos para disminuir el número de tontas en el mundo.",
+        "Go for details": "Más información",
+    },
+    DetailContext: {
+        Details: "详细信息",
+        "Before you submit, you need to make sure your situation meets our requirements. We have the following requirements for submitted applications:":
+            "Para las solicitudes presentadas, tenemos los siguientes requisitos:",
+        "You must open source the code of the applications you deploy using the subdomain under any license.":
+            "Debe fuente abierta sus aplicaciones (cualquier licencia es aceptable)",
+        "You may not monetize the applications deployed by the subdomain in any way, except for non-mandatory donations.":
+            "No puedes ganar dinero desde este proyecto.",
+        "You are not allowed to deploy service projects such as IDC and Image Storage Service in subdomains. However, display sites such as personal blogs are allowed.":
+            "Sólo puede implementar un sitio web personal en el subdominio",
+        "Subdomains with specific meanings such as www, ftp, blog, etc. cannot be applied for.":
+            "No se pueden solicitar subdominios con significados específicos tales, por ejemplo: www, ftp, blog, etc.",
+        "You need to keep your applications running for a long time. In the event of multiple service interruptions, we will cancel the resolution of your subdomain.":
+            "Necesitas mantener tu subdominio disponible por mucho tiempo. Si el servicio no está disponible a menudo, ya no podrá utilizar subdominios",
+        "You must deploy your applications legally, not from your home PC.":
+            "Debe desplegar sus aplicaciones normalmente, por ejemplo, sar su computadora privado no es aceptable",
+        "There are strict formatting requirements to follow for your application:":
+            "Debes estar en el siguiente formato:",
+        "You need to point out what the idiot did, write it as best you can!":
+            "apunta lo que hizo el tontas",
+        "You need to identify yourself and leave your contact details so we can better communicate with you.":
+            "Identifíquese y deje sus datos de contacto",
+        "If authenticated, you have unlimited access to subdomains which begin with your name, which is discouraged but considered cool.":
+            "Si su aplicación pasó, podrá obtener subdominios ilimitados que comiencen con su nombre, aunque no te animamos a hacerlo",
+        "The application format is as follows:": "Formatos de aplica:",
+        "Your name": "Su nombre",
+        "Your contact": "Sus datos de contacto",
+        "Your project name": "Nombre de su proyecto",
+        "The type of subdomain you are applying for": "El tipo de subdominio que está solicitando (NS/CNAME)",
+        "What idiots do (detailed description)": "que hizo el tonta (cuanto más detallado mejor)",
+        "Idiots' contact": "Datos de contacto del tonta",
+        "Go for apply": "Enviar la solicitud",
+        Confirm: "Constatar",
+        "You will be navigated to the GitHub Issue page, be sure to state [Apply] at the beginning of the title of the Issue":
+            "Se le redirigirá a la página de GitHub Issue, el estado [Apply] al comienzo del título de la Issue es necesario.",
+        Cancel: "Anular",
+        Go: "Go",
+    },
+    pageName: {
+        Home: "主页",
+        Details: "详细信息",
+        "Fuck Your Mom": "Fuck Your Mom",
+    },
+    siteCard: {
+        Site: "Site: ",
+        Domain: "Dominio: ",
+        Owner: "Dueño: ",
+        Idiot: "Tonta: ",
+        Go: "Go: ",
+    },
+    languageSwitcher: {
+        "System language switched to {{name}}": "Idioma actualmente: {{name}}",
+    },
+};
+
+export default esMX;
